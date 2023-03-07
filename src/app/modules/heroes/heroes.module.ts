9706747b2eff714d5c23583fb31fc28722466dba
Juan José García <jitos@jitos.dev>
2023-03-07 10:22:08 +0100

    generate disctint componets in module heroes

--- src/app/modules/heroes/heroes.module.ts
+++ src/app/modules/heroes/heroes.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AltaComponent } from './pages/alta/alta.component';
+import { BusquedaComponent } from './pages/busqueda/busqueda.component';
+import { DetalleComponent } from './pages/detalle/detalle.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListadoComponent } from './pages/listado/listado.component';
+import { ModalComponent } from './pages/modal/modal.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AltaComponent,
+    BusquedaComponent,
+    DetalleComponent,
+    HomeComponent,
+    ListadoComponent,
+    ModalComponent
+  ],
   imports: [
     CommonModule
   ]
