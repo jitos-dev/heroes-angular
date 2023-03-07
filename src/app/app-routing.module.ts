6fdb7b95bad3931be9aa8874fef8112498c8609e
Juan José García <jitos@jitos.dev>
2023-03-07 12:10:05 +0100

    create principal component home.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeHeroesComponent } from './modules/heroes/pages/home/home-heroes.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/pages/home/home.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'heroes',
     loadChildren: () => import("./modules/heroes/heroes.module").then(m => m.HeroesModule)
